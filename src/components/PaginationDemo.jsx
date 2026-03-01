c5d0d38017f1dfc845ea76f7a82f5952d9066b47
Rod Gaitan <rodgaitan01@gmail.com>
2026-02-28 21:24:14 -0500

    commit working on Paginaton

---
+++ src/components/PaginationDemo.jsx
@@ -0,0 +1,17 @@
+import React, { useSate } from React; 
+
+function usePagination(totalItems, itemsPerPage, initialPage)
+
+   // const [count, setCount] = useState(0);
+  
+
+
+
+
+
+
+
+
+
+
+export default usePagination;
