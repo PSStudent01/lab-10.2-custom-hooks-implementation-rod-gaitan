ef08fa9f4a63553eca5ace4ec319b310dc341f74
Rod Gaitan <rodgaitan01@gmail.com>
2026-02-28 23:18:37 -0500

    commit working on Paginaton

--- src/components/PaginationDemo.jsx
+++ src/components/PaginationDemo.jsx
@@ -1,22 +1,24 @@
 import React, { useSate } from React; 
 
-function usePagination(totalItems, itemsPerPage, initialPage)
+// takes input for 3 needed parameters
+function usePagination(totalItems, itemsPerPage, initialPage) {
 
+    //tracks/remembers the page you're on
    const [currentPage, setcurrentPage] = useState(0);
   
-   // total pages
+   //calculates total pages
     const totalPages = Math.ceil(totalItems / itemsPerPage)
     //console.log(totalPages)
 
-    const currentPage < totalPages && currentPage > 1
-    
-    const startIndex =
+    //calculates what page item you'd start at in the current page
+    const startIndex = (currentPage - 1) * itemsPerPage; //assuming: 25 items total;  10 items per page; 2.5 total pages; 
+                                                        // if current page is 2, then you would start at page item 10:  (2 - 1) * 10 = 10 as your 'startIndex'
+    const endIndex = Math.min(startIndex + itemsPerPage, totalItems); // so now 'startIndex' = 10; itemsPerPage = 10:  (10 + 10) = 20 as your 'endIndex'/end item
+=
+    //calculates how many Items Are on Current Page
+    const itemsOnCurrentPage = endIndex - startIndex 
 
-    const endIndex =
-
-    const itemsOnCurrentPage =
-
-
+}
 
 
 export default usePagination;
